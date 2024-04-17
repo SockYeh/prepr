6e005b59f7caa63ae8cf23c03e251e41a710bd5f
Sahran <82019509+SockYeh@users.noreply.github.com>
2024-04-17 22:24:34 +0530

    Add CORS middleware

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routes import auth, problems, comments
 from .utils.database_handler import (
@@ -12,6 +13,8 @@ from .utils.database_handler import (
     create_problems_db,
     create_comments_db,
 )
+
+origins = ["http://localhost:8000", "http://localhost:3000"]
 
 
 @asynccontextmanager
@@ -25,6 +28,14 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(SessionMiddleware, secret_key=os.environ["SECRET"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 routers = [auth.router, problems.router, comments.router]
 for router in routers:
