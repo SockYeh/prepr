1f5777487fa268dd0a803f4993231215ae84fb18
Sahran <82019509+SockYeh@users.noreply.github.com>
2024-04-17 21:44:13 +0530

    Fix post comments

--- backend/app/api/routes/comments.py
+++ backend/app/api/routes/comments.py
@@ -1,4 +1,4 @@
-import aiohttp, os, logging
+import aiohttp, os, logging, pydantic
 from dotenv import load_dotenv, find_dotenv
 from fastapi import APIRouter, HTTPException, status, Request, Depends
 from fastapi.responses import JSONResponse
@@ -6,7 +6,6 @@ from starlette.responses import RedirectResponse
 from ..utils.database_handler import (
     create_comment,
     get_comments,
-    CommentModel,
     get_comment,
     update_comment,
     delete_comment,
@@ -17,6 +16,15 @@ load_dotenv(find_dotenv())
 
 router = APIRouter(prefix="/comments", tags=["comments"])
 logger = logging.getLogger(__name__)
+
+
+class CommentForm(pydantic.BaseModel):
+    user: str
+    comment: str
+    problem: str
+    likes: int = 0
+
+    model_config = {"arbitrary_types_allowed": True}
 
 
 @router.get("/", response_class=JSONResponse, status_code=status.HTTP_200_OK)
@@ -33,7 +41,7 @@ async def get_comments_ep(request: Request, problem_id: str):
     status_code=status.HTTP_200_OK,
     dependencies=[Depends(is_logged_in)],
 )
-async def add_comment_ep(request: Request, comment: CommentModel):
+async def add_comment_ep(request: Request, comment: CommentForm):
     op = await create_comment(**comment.model_dump())
     return JSONResponse(content={"message": f"Added comment with ID: {op}"})
 
@@ -44,7 +52,7 @@ async def add_comment_ep(request: Request, comment: CommentModel):
     status_code=status.HTTP_200_OK,
     dependencies=[],  # HELP!
 )
-async def update_comment_ep(request: Request, comment_id: str, comment: CommentModel):
+async def update_comment_ep(request: Request, comment_id: str, comment: CommentForm):
     op = await update_comment(comment_id, **comment.model_dump())
     return JSONResponse(content={"message": f"Comment updated with ID: {op}"})
 
